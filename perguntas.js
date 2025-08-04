a2c3897598b639feab99af4dabe109817f49ad19
Larissa9512 <medeiro.santos@escola.pr.gov.br>
2025-08-04 12:04:43 -0300

    index

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Onde se localiza o egito?',
+    'O Egito está localizado na África'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital do brasil ?',
+    'A capital do brasil é brasilia'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Qual o país com mais ilhas no mundo?',
+    ' é a Suécia'
 )
     criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
+    'geografia',
+    'Qual é o rio mais longo do mundo?',
+    'O rio Nilo'
 )
